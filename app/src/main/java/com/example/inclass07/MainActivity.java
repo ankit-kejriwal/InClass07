bf34243e69de63bccdd325fc836c47f3a2ea45d5
ankit <ankitkejriwaal@gmail.com>
2019-10-14 19:25:51 -0400

    added element name in the main file

--- app/src/main/java/com/example/inclass07/MainActivity.java
+++ app/src/main/java/com/example/inclass07/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.inclass07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    TextView textViewlimit;
+    SeekBar seekBarLimit;
+    Button buttonSearch;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        textViewlimit = findViewById(R.id.textViewlimit);
+        seekBarLimit = findViewById(R.id.seekBarLimit);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        radioButton = findViewById(R.id.radioButton);
+
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo == null || !networkInfo.isConnected() ||
+                (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                        && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+            return false;
+        }
+        return true;
     }
 }
